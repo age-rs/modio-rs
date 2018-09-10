37ba29e747ff06331ca3491a583e1e626454ebc1
Constantin Nickel <constantin.nickel@gmail.com>
2018-09-10 05:18:37 +0000

    rebuild pages at 4b4af5fd0735e610dddb98e0c12d835822311a2f

--- master/modio/me/sidebar-items.js
+++ master/modio/me/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Me","Interface for resources owned by the authenticated user or is team member of."],["SubscriptionsListOptions","Options used to filter subscription listings."]]});
+initSidebarItems({"enum":[["Rating","See the Rating Object docs for more informations."]],"struct":[["Me","Interface for resources owned by the authenticated user or is team member of."],["RatingsListOptions","Options used to filter rating listings."],["SubscriptionsListOptions","Options used to filter subscription listings."]]});
