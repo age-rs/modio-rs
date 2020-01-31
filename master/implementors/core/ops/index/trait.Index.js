2a3343aa33ad292fdb76785322a0170d39f70fba
nickelc <nickelc@users.noreply.github.com>
2020-01-31 01:33:08 +0000

    Automated deployment: Fri Jan 31 01:33:08 UTC 2020 340b8dab25985c37c39108e8e4027eefa713cc12

--- master/implementors/core/ops/index/trait.Index.js
+++ master/implementors/core/ops/index/trait.Index.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["modio"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"modio/struct.List.html\" title=\"struct modio::List\">List</a>&lt;T&gt;",synthetic:false,types:["modio::types::List"]},];
+implementors["modio"] = [{"text":"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"modio/struct.List.html\" title=\"struct modio::List\">List</a>&lt;T&gt;","synthetic":false,"types":["modio::types::List"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
