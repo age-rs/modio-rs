8e698d13274f04a8f0abc824189c19ed41a26ccc
Constantin Nickel <constantin.nickel@gmail.com>
2020-06-05 13:39:21 +0200

    Bump version

--- src/lib.rs
+++ src/lib.rs
@@ -108,7 +108,7 @@
 //! #    Ok(())
 //! # }
 //! ```
-#![doc(html_root_url = "https://docs.rs/modio/0.4.1")]
+#![doc(html_root_url = "https://docs.rs/modio/0.5.0")]
 #![deny(rust_2018_idioms)]
 #![deny(intra_doc_link_resolution_failure)]
 
